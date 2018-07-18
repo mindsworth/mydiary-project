aa0418f2546986482fce9ad79b863b071ce17456
mindsworth <princegoziem@gmail.com>
2018-07-18 17:14:55 +0100

    Test Fix and README updated commit

--- server/test/entries.test.js
+++ server/test/entries.test.js
@@ -32,6 +32,17 @@ describe('Test entry routes', () => {
       });
   });
 
+  it('should get a specific entry', (done) => {
+    chai.request(app)
+      .get('/api/v1/entries/3')
+      .end((err, res) => {
+        const { message } = res.body;
+        if (err) return done(err);
+        expect(res.statusCode).to.equal(404);
+        expect(message).to.equal(`The entry with the ID 3 is not found.`);
+        return done();
+      });
+  });
   it('should add a new entry', (done) => {
     chai.request(app)
       .post('/api/v1/entries')
@@ -42,12 +53,29 @@ describe('Test entry routes', () => {
       ))
       .end((err, res) => {
         const { entries } = res.body;
-        console.log(entries);
         if (err) return done(err);
         expect(res.statusCode).to.equal(201);
         expect(entries.length).to.equal(3);
         expect(entries[2].title).to.equal('Jenifa\'s Diary');
-        console.log('COUNT:', entries.length);
+        return done();
+      });
+  });
+
+  it('should return status code 400 and a message when title'
+    + ' and description is not given', (done) => {
+    chai.request(app)
+      .post('/api/v1/entries')
+      .send(entrySeeder.setEntryData(
+        3,
+        '',
+        '',
+      ))
+      .end((err, res) => {
+        const { Message } = res.body;
+        if (err) return done(err);
+        expect(res.statusCode).to.equal(400);
+        expect(Message).to.equal('title and description Fields'
+        + ' should not be Empty');
         return done();
       });
   });
@@ -88,24 +116,6 @@ describe('Test entry routes', () => {
       });
   });
 
-  it('should return status code 400 and a message when title'
-    + ' and description is not given', (done) => {
-    chai.request(app)
-      .post('/api/v1/entries')
-      .send(entrySeeder.setEntryData(
-        3,
-        '',
-        '',
-      ))
-      .end((err, res) => {
-        const { Message } = res.body;
-        if (err) return done(err);
-        expect(res.statusCode).to.equal(400);
-        expect(Message).to.equal('title Field should not be Empty');
-        return done();
-      });
-  });
-
   it('should modifiy a specific entry', (done) => {
     chai.request(app)
       .put('/api/v1/entries/1')
@@ -140,6 +150,24 @@ describe('Test entry routes', () => {
       });
   });
 
+  it('should return status code 404 and a message when the'
+    + ' entry is not found', (done) => {
+    chai.request(app)
+      .put('/api/v1/entries/2')
+      .send(entrySeeder.setEditEntryData(
+        '',
+        'What if its you with someone else\'s future wife?',
+      ))
+      .end((err, res) => {
+        const { Message } = res.body;
+        console.log(Message);
+        if (err) return done(err);
+        expect(res.statusCode).to.equal(400);
+        expect(Message).to.equal('title Field should not be Empty');
+        return done();
+      });
+  });
+
   it('should delete a specific entry', (done) => {
     chai.request(app)
       .delete('/api/v1/entries/2')
@@ -148,6 +176,18 @@ describe('Test entry routes', () => {
         if (err) return done(err);
         expect(res.statusCode).to.equal(200);
         expect(RemainingEntries.length).to.equal(2);
+        return done();
+      });
+  });
+
+  it('should delete a specific entry', (done) => {
+    chai.request(app)
+      .delete('/api/v1/entries/4')
+      .end((err, res) => {
+        const { Message } = res.body;
+        if (err) return done(err);
+        expect(res.statusCode).to.equal(404);
+        expect(Message).to.equal('Entry does not exist');
         return done();
       });
   });
