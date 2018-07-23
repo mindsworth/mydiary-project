23ac6bdf30e96ce52af47890714f9e25a6b67f88
mindsworth <princegoziem@gmail.com>
2018-07-23 12:41:19 +0100

    gh-pages-UI commit

--- js/custom.js
+++ js/custom.js
@@ -3,21 +3,21 @@ const userDropdown = document.querySelector('.user-dropdown');
 const categoryBtn = document.querySelector('.modal-open');
 const modalOlay = document.querySelector('.modal-olay');
 const modalClose = document.querySelector('.modal-close');
+const listDropdowns = document.querySelectorAll('.fa-ellipsis-h');
 
-let show = false;
+let showUserDropdown = false;
+let showEntryDrawer = false;
 
 
 
 
 const displayDropdownHandler = () => {
-    if (show) {
+    if (showUserDropdown) {
         userDropdown.style.display = 'none';
-        show = !show;
-        console.log(show);
+        showUserDropdown = !showUserDropdown;
     } else {
         userDropdown.style.display = 'block';
-        show = !show;
-        console.log(show);
+        showUserDropdown = !showUserDropdown;
     }
 }
 
@@ -30,6 +30,20 @@ const categoryModalHideHandler = () => {
     modalOlay.style.display = 'none';
 }
 
+[].forEach.call(listDropdowns, (drop) => {
+    drop.addEventListener('click', function () {
+        if (showEntryDrawer) {
+            drop.style.background = "none";
+            this.parentNode.className = "edit";
+            showEntryDrawer = !showEntryDrawer;
+
+        } else {
+            drop.style.background = "#eee";
+            this.parentNode.className = "edit show-drawer";
+            showEntryDrawer = !showEntryDrawer;
+        }
+    })
+});
 
 userPic.addEventListener('click', displayDropdownHandler);
 categoryBtn.addEventListener('click', categoryModalShowHandler);
