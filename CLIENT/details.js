5ba049e685c5eca76c8fe3d67845961450c081af
mindsworth <princegoziem@gmail.com>
2018-08-11 05:39:47 +0100

    feat(details-page-functionality): details page frontend functionality implemented
    On this page users can view the details of an entry
    
    [Finishes #159647442]

--- CLIENT/details.js
+++ CLIENT/details.js
@@ -8,9 +8,10 @@ const MakeNetworkRequest = (input = {
     mode: 'cors',
   };
 
-  if (input.method === 'get') {
+  if (input.method === 'get' || input.method === 'delete') {
     reqObject.headers = {
       'content-type': 'application/json',
+      'x-access-token': input.data.token,
     };
   } else {
     reqObject.headers = {
@@ -24,9 +25,74 @@ const MakeNetworkRequest = (input = {
     .catch(err => err);
 };
 
+const redirect = (response) => {
+  if (response.newEntry) {
+    localStorage.setItem('entry_id', response.newEntry[0].entry_id);
+    window.location.href = 'details.html';
+  }
+};
+
+const formatDate = (nDate) => {
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  const d = nDate.split('T')[0];
+  const date = new Date(d)
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const formatedDate = `${month} ${day}, ${year}`;
+  return formatedDate;
+}
+
+const timeSince = function (date) {
+  if (typeof date !== 'object') {
+    date = new Date(date);
+  }
+
+  const seconds = Math.floor((new Date() - date) / 1000);
+  let intervalType;
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = 'year';
+  } else {
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      intervalType = 'month';
+    } else {
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = 'day';
+      } else {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+          intervalType = "hour";
+        } else {
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = "minute";
+          } else {
+            interval = seconds;
+            intervalType = "second";
+          }
+        }
+      }
+    }
+  }
+
+  if (interval > 1 || interval === 0) {
+    intervalType += 's';
+  }
+
+  return `${interval}${intervalType} ago`;
+};
+
+let entryId;
+
 class DetailsClient {
   init() {
     new DetailsClient().getSingleEntry();
+    new DetailsClient().handleModal();
+    new DetailsClient().deleteEntry();
   }
 
   checkToken() {
@@ -38,20 +104,95 @@ class DetailsClient {
     return token;
   }
 
-  getSingleCategory() {
+  handEditIcon() {
+    const pencil = document.querySelector('.fa-pencil-alt');
 
+    pencil.addEventListener('click', () => {
+      localStorage.setItem('entry_id', '');
+    });
+  }
+
+  handleModal() {
+    const trash = document.querySelector('.fa-trash-alt');
+    const modalOlay = document.querySelector('.modal-olay');
+    const modalClose = document.querySelector('.cancel');
+
+    trash.addEventListener('click', () => {
+      modalOlay.style.display = 'flex';
+    });
+
+    modalClose.addEventListener('click', () => {
+      modalOlay.style.display = 'none';
+    });
+  }
+
+  getSingleCategory(categoryId) {
+    const category = document.querySelector('.category');
+    const color = document.querySelector('.color');
+    const colors = ['#0AD3FC', '#200AFC', '#CC10D9', '#FC0A3C', '#5AFC0A'];
+    const token = new DetailsClient().checkToken();
+    const data = {
+      token,
+    }
+
+    const url = `https://chigoziem-mydiary-bootcamp-app.herokuapp.com/api/v1/categories/${categoryId}`;
+    const method = 'get';
+    MakeNetworkRequest({
+        url,
+        method,
+        data
+      }).then((response) => {
+        category.innerHTML = response.category[0].title;
+        color.style.backgroundColor = colors[response.category[0].color_id];
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  deleteEntry() {
+    const accept = document.querySelector('.accept');
+
+    accept.addEventListener('click', () => {
+      console.log('Hello');
+
+      const token = new DetailsClient().checkToken();
+      const data = {
+        token,
+      }
+      const entryId = localStorage.getItem('entry_id');
+      const url = `https://chigoziem-mydiary-bootcamp-app.herokuapp.com/api/v1/entries/${entryId}`;
+      const method = 'delete';
+
+      MakeNetworkRequest({
+          url,
+          method,
+          data
+        }).then((response) => {
+          if (response.message === 'Invalid authorization token') {
+            window.location.href = 'login.html';
+          }
+          localStorage.setItem('entry_id', '');
+          window.location.href = 'dashboard.html';
+          console.log(response)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
   }
 
   getSingleEntry() {
     const title = document.querySelector('.title');
-    const category = document.querySelector('.category');
+    const createdAt = document.querySelector('.createdat');
+    const updatedAt = document.querySelector('.updatedat');
     const description = document.querySelector('.description');
     const token = new DetailsClient().checkToken();
     const data = {
       token,
     }
     const entryId = localStorage.getItem('entry_id');
-    const url = `https://chigoziem-mydiary-bootcamp-app.herokuapp.com/api/v1/entries/${entryId}?token=${token}`;
+    const url = `https://chigoziem-mydiary-bootcamp-app.herokuapp.com/api/v1/entries/${entryId}`;
     const method = 'get';
     MakeNetworkRequest({
         url,
@@ -61,16 +202,14 @@ class DetailsClient {
         if (response.message === 'Invalid authorization token') {
           window.location.href = 'login.html';
         }
-        console.log(response);
         title.innerHTML = response.entry[0].title;
-        category.innerHTML = response.entry[0].category;
+        entryId = response.entry[0].entry_id;
+        const categoryId = response.entry[0].category_id;
         description.innerHTML = response.entry[0].description;
-        // response.categories.map((item) => {
-        //   const opt = document.createElement('option');
-        //   opt.setAttribute('value', item.category_id);
-        //   opt.innerText = item.title;
-        //   category.append(opt);
-        // });
+        new DetailsClient().getSingleCategory(categoryId);
+        createdAt.innerHTML = formatDate(response.entry[0].createdat);
+        updatedAt.innerHTML = timeSince(new Date(response.entry[0].updatedat));
+        redirect(response);
       })
       .catch((err) => {
         console.log(err);
