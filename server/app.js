c996ab2a6b7f2baf12455db4d6f654b43b28f10f
mindsworth <princegoziem@gmail.com>
2018-08-29 05:56:05 +0100

    add documentation file

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes/index';
+import swaggerDoc from '../swagger.json';
 import cronMail from './middleware/mail-sender';
 
 
@@ -20,6 +22,7 @@ app.use(cors());
 cronMail.start();
 routes(router);
 app.use('/api/v1/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.get('*', (req, res) => res.status(404).json({
   message: 'Request Not Found!',
