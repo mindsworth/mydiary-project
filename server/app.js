2a2f55a9356fb428874df4739124ab25e9709e91
Prince Goziem <chigoziem.nwaiwu@andela.com>
2019-10-16 19:07:49 +0100

    - update all dependencies re-configue prettier with eslint and babel

--- server/app.js
+++ server/app.js
@@ -1,31 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import routes from './routes/index';
-import swaggerDoc from '../swagger.json';
-import cronMail from './middleware/mail-sender';
+import express from 'express'
+import bodyParser from 'body-parser'
+import logger from 'morgan'
+import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import routes from './routes/index'
+import swaggerDoc from '../swagger.json'
+import cronMail from './middleware/mail-sender'
 
+const app = express()
+const router = express.Router()
 
-const app = express();
-const router = express.Router();
+app.use(logger('dev'))
 
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.use(bodyParser.urlencoded({
-  extended: false,
-}));
-app.use(bodyParser.json());
-app.use(cors());
+cronMail.start()
+routes(router)
+app.use('/api/v1/', router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
-cronMail.start();
-routes(router);
-app.use('/api/v1/', router);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.get('*', (req, res) =>
+	res.status(404).json({ message: 'Request Not Found!' })
+)
 
-app.get('*', (req, res) => res.status(404).json({
-  message: 'Request Not Found!',
-}));
-
-export default app;
+export default app
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-import http from 'http';
-import app from './app';
+import http from 'http'
+import app from './app'
+import Sequelize from './database/models'
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 server.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
-});
+	console.log(`Server listening at port ${port}`)
+})
 
-export default server;
+export default server
